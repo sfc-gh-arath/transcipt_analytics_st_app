d0ddb094229bfac92e81757164b577e8bea62b4e
Aswinee Rath <aswinee.rath@snowflake.com>
2025-08-13 20:56:29 -0700

    st.page changes

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import streamlit as st
 
-st.title('Multi-page app demo')
-st.write('This is the landing page for the app. Click in the left sidebar to open other pages in the app')
+Agent_Performance_Dashboard = st.Page("Agent_Performance_Dashboard.py.py", title="Agent_Performance_Dashboard" )
+Individual_Agent_Scorecard = st.Page("Individual_Agent_Scorecard.py", title="Individual_Agent_Scorecard" )
+Individual_Call_Review_Dashboard = st.Page("Individual_Call_Review_Dashboard.py", title="Individual_Call_Review_Dashboard" )
+
+
+pg = st.navigation([Agent_Performance_Dashboard, Individual_Agent_Scorecard,Individual_Call_Review_Dashboard])
+st.set_page_config(page_title="Call Transcript Analytcis", page_icon=":material/edit:")
+pg.run()
