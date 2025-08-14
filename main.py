7eb3b2b62561cadea00c1c0dae302d040711676c
Aswinee Rath <aswinee.rath@snowflake.com>
2025-08-13 20:57:39 -0700

    bug fixes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-Agent_Performance_Dashboard = st.Page("Agent_Performance_Dashboard.py.py", title="Agent_Performance_Dashboard" )
+Agent_Performance_Dashboard = st.Page("Agent_Performance_Dashboard.py", title="Agent_Performance_Dashboard" )
 Individual_Agent_Scorecard = st.Page("Individual_Agent_Scorecard.py", title="Individual_Agent_Scorecard" )
 Individual_Call_Review_Dashboard = st.Page("Individual_Call_Review_Dashboard.py", title="Individual_Call_Review_Dashboard" )
 
